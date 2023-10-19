0855a761f4f76d1500dc9e3d09c664e158cef271
liujin <liujin@elinklaw.com>
2023-10-19 17:39:32 +0800

    init

---
+++ build/App.mjs
@@ -0,0 +1,7 @@
+import { j as r } from "./node_modules/react/jsx-runtime.mjs";
+function e() {
+  return /* @__PURE__ */ r.jsx(r.Fragment, { children: "hello world" });
+}
+export {
+  e as default
+};
