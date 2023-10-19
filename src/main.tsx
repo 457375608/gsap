a12ad2f7306fc61f18a9777201ff0b182bda1f4e
liujin <liujin0505@icloud.com>
2023-10-19 21:27:40 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,14 @@
-// import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import App from "./lib.tsx";
 
-// ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <App
+    size={[400, 400]}
+    position={[121.506379, 31.245414]}
+    info={{
+      title: "东方明珠广播电视塔",
+      address: "地址：上海市浦东新区世纪大道1号",
+      width: 240,
+    }}
+  />
+);
