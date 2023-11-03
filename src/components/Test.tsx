ac21a82477e7d1bdb78ac9d3d63cfc7c09895a09
liujin <liujin0505@icloud.com>
2023-11-03 15:20:49 +0800

    feat: 增加svg loader

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,14 +1,15 @@
 import copyText from '@/utils/copy'
+import Like from '@/assets/icons/like.svg?react'
 
 const index = () => {
   return (
     <div
-      className="w-full h-10 bg-gray-100 flex justify-center items-center cursor-pointer"
+      className="w-full h-10 bg-gray-100 text-purple-600 flex justify-center items-center cursor-pointer"
       onClick={() => {
         copyText('123').then((result) => console.log(result))
       }}
     >
-      点击测试copyText函数
+      <Like></Like>点击测试copyText函数
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "node:path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
+import svgLoader from 'vite-svg-loader'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+    svgLoader({
+      defaultImport: 'url',
+    }),
+  ],
   resolve: {
     //路径别名
     alias: {
-      "~": path.resolve(__dirname, "./"), // 根路径
-      "@": path.resolve(__dirname, "src"), // src 路径
+      '~': path.resolve(__dirname, './'), // 根路径
+      '@': path.resolve(__dirname, 'src'), // src 路径
     },
   },
-});
+})
